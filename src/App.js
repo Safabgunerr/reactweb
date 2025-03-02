f149465cb992b3cfc8baa4ed52243d3bf3b8a2a8
safabgunerr <safabguner54@gmail.com>
2025-03-02 21:35:59 +0300

    [SAFA] - giriş yazısını değiştirdim

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ARCHİS ACADEMY GİT GİTHUB
+          Safa Git ve Github
         </p>
         <a
           className="App-link"
